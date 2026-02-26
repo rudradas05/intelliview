43ac16bf64750ae9b3cb584848e9286bd3b95496
rudradas05 <dasrudra.stu@gmail.com>
2026-02-26 19:34:54 +0530

    update Next.js configuration and Prisma schema; add Vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  serverExternalPackages: ["pdf-parse", "@prisma/client"],
+  turbopack: {},
 };
 
 export default nextConfig;
